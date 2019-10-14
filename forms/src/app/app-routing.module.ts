ab883e8a37e3d984983b216bc9422d3ca92641ef
leanfj <leanfj@hotmail.com>
2019-10-14 00:11:12 -0300

    Criando arquivos de controle de rotas

--- forms/src/app/app-routing.module.ts
+++ forms/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { DataFormComponent } from './data-form/data-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'templateForm', component: TemplateFormComponent },
+  { path: 'dataForm', component: DataFormComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'templateForm' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
