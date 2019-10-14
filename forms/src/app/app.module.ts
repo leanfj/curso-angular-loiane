2e0817578c0a91c087d725a5a423705ccc2425ef
leanfj <leanfj@hotmail.com>
2019-10-14 00:33:36 -0300

    Separando em modulos

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -5,13 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
 
 @NgModule({
-  declarations: [AppComponent, TemplateFormComponent, DataFormComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, DataFormComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, TemplateFormModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
