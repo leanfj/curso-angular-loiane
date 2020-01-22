770a9383c7c3c6144aa0ee0f66c8c2775bd79e6b
leanfj <leanfj@hotmail.com>
2020-01-22 00:34:03 -0300

    Criando template basico do app

--- http-request/src/app/app-routing.module.ts
+++ http-request/src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'cursos'
+  },
+  {
+    path: 'cursos',
+    loadChildren: './cursos/cursos.module#CursosModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
