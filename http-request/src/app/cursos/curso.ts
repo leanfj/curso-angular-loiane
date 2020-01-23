b4cbaf99b33083a78b2446a655aa4507203f1768
leanfj <leanfj@hotmail.com>
2020-01-22 22:36:04 -0300

    Show data in table via service request with http

---
+++ http-request/src/app/cursos/curso.ts
@@ -0,0 +1,4 @@
+export interface Curso {
+  id: number,
+  name: string
+}
---
+++ http-request/src/app/cursos/cursos.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Curso } from './curso';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  private readonly API = 'http://localhost:3000/cursos'
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  list() {
+    return this.http.get<Curso[]>(this.API).pipe(tap(console.log))
+  }
+}
