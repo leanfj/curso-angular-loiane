69d4e110bd36935b652de6e7e7aa1efba1fa6e8f
leanfj <leanfj@hotmail.com>
2020-01-30 07:58:51 -0300

    Criado form para cadastro de curso

--- http-request/src/app/cursos/cursos-routing.module.ts
+++ http-request/src/app/cursos/cursos-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
+import { CursosFormComponent } from './cursos-form/cursos-form.component';
 
 
-const routes: Routes = [{
-  path: '', component: CursosListaComponent
-}];
+const routes: Routes = [
+  { path: '', component: CursosListaComponent },
+  { path: 'novo', component: CursosFormComponent },
+  { path: 'editar/:id', component: CursosFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- http-request/src/app/cursos/cursos.module.ts
+++ http-request/src/app/cursos/cursos.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CursosRoutingModule } from './cursos-routing.module';
 import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
+import { CursosFormComponent } from './cursos-form/cursos-form.component';
 
 
 @NgModule({
-  declarations: [CursosListaComponent],
+  declarations: [CursosListaComponent, CursosFormComponent],
   imports: [
     CommonModule,
-    CursosRoutingModule
+    CursosRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class CursosModule { }
